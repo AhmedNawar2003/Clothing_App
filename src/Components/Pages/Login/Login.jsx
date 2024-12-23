886d37ca80d22f623e36b64a63289057f0fe0edc
a-elshennawy <116855413+a-elshennawy@users.noreply.github.com>
2024-12-23 04:24:28 +0200

    adjusting login?register (design only)

--- src/Components/Pages/Login/Login.jsx
+++ src/Components/Pages/Login/Login.jsx
@@ -1,8 +1,26 @@
+import "./login.css"
+import Switchers from "../../Switch_log/Switchers"
 
 export default function Login() {
-  return (
-    <div>
-      <h1>Login</h1>
+  return <>
+    <div className="container">
+      <Switchers />
+      <div className="row logIn">
+        <div className="form-floating mb-3">
+          <input type="email" className="form-control" id="floatingemail" placeholder="email" />
+          <label for="floatingemail">email address *</label>
+        </div>
+        <div className="form-floating mb-3">
+          <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+          <label for="floatingPassword">Password *</label>
+        </div>
+        <button className="col-12 logBtn">
+          log in
+        </button>
+        <div className="col-12 toRegister">
+          <p>no account yet ? <a href="">create account</a></p>
+        </div>
+      </div>
     </div>
-  )
+  </>
 }
--- src/Components/Pages/Register/Register.jsx
+++ src/Components/Pages/Register/Register.jsx
@@ -1,8 +1,30 @@
+import "./Register.css"
+import Switchers from "../../Switch_log/Switchers"
 
 export default function Register() {
-  return (
-    <div>
-      <h1>Register</h1>
+  return <>
+    <div className="container">
+      <Switchers />
+      <div className="row register">
+        <div className="form-floating mb-3">
+          <input type="username" className="form-control" id="floatingInput" placeholder="name@example.com" />
+          <label for="floatingInput">Username</label>
+        </div>
+        <div className="form-floating mb-3">
+          <input type="email" className="form-control" id="floatingemail" placeholder="email" />
+          <label for="floatingemail">email address *</label>
+        </div>
+        <div className="form-floating mb-3">
+          <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+          <label for="floatingPassword">Password *</label>
+        </div>
+        <div className="col-12">
+          <p>Your personal data will be used to support your experience throughout this website, to manage access to your account, and for other purposes described in our privacy policy.</p>
+        </div>
+        <button className="col-12 regBtn">
+          register
+        </button>
+      </div>
     </div>
-  )
+  </>
 }
