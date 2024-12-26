931ad843c7c4fdcd61fcda0d88eba57913f80012
a-elshennawy <116855413+a-elshennawy@users.noreply.github.com>
2024-12-26 19:33:12 +0200

    trending products basic structure

--- src/Components/Pages/Home/TrendyProducts/Trendy.jsx
+++ src/Components/Pages/Home/TrendyProducts/Trendy.jsx
@@ -1,21 +1,27 @@
-import React from 'react'
+import "./trendy.css"
 
 export default function Trendy() {
     return <>
-        <div className="header">
-            <h2>Our Trendy <span>products</span></h2>
-        </div>
-        <div className="nav">
-            <button className="btn">all</button>
-            <button className="btn">new arrivals</button>
-            <button className="btn">best seller</button>
-            <button className="btn">top rated</button>
-        </div>
-        <div className="products">
+        <section>
+            <div className="container text-center">
+                <div className="row trendy">
+                    <div className="header">
+                        <h2>Our Trendy <span>products</span></h2>
+                    </div>
+                    <div className="navBtns">
+                        <button className="btn">all</button>
+                        <button className="btn">new arrivals</button>
+                        <button className="btn">best seller</button>
+                        <button className="btn">top rated</button>
+                    </div>
+                    <div className="row products">
 
-        </div>
-        <div className="more">
-            <button className="btn">discover more</button>
-        </div>
+                    </div>
+                    <div className="more">
+                        <button className="btn">discover more</button>
+                    </div>
+                </div>
+            </div>
+        </section>
     </>
 }
