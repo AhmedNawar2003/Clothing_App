acbe08c2ae96b6436b03bfc48910d66f9f21f9fb
a-elshennawy <116855413+a-elshennawy@users.noreply.github.com>
2024-12-27 06:20:53 +0200

    passing data betn shop & details

--- src/Components/Pages/Shop/Shop.jsx
+++ src/Components/Pages/Shop/Shop.jsx
@@ -1,18 +1,26 @@
 import React, { useState, useEffect } from "react";
 import { Helmet, HelmetProvider } from "react-helmet-async";
+import { useNavigate } from 'react-router-dom';
 import "./Shop.css";
 
 export default function Shop() {
-  // State to store fetched products
   const [products, setProducts] = useState([]);
+  const navigate = useNavigate();
 
-  // Fetch products from products.json when the component mounts
+
   useEffect(() => {
     fetch("/products.json")
-      .then((response) => response.json())  // Convert the response to JSON
-      .then((data) => setProducts(data))    // Update state with the fetched data
+      .then((response) => response.json())
+      .then((data) => {
+        const filteredProducts = data.filter(product => product["limited-edition"] === false);
+        setProducts(filteredProducts);
+      })
       .catch((error) => console.error("Error fetching products:", error));
   }, []);
+
+  const handleProductClick = (product) => {
+    navigate('/Details', { state: { product } });
+  };
 
   return (
     <>
@@ -30,16 +38,19 @@ export default function Shop() {
           <div className="row products">
             {products.map((product) => (
               <div key={product.id} className="col-3 item">
-                <div className="img">
-                  <img
+                <div className="img" onClick={() => handleProductClick(product)}>
+                  <img className="firstImage"
                     src={product.image_1}
                     alt={product.name}
                   />
+                  <img className="seccondImage"
+                    src={product.image_2}
+                    alt={product.name} />
                   <button className="addtoCart">add to cart</button>
                 </div>
                 <div className="info">
-                  <p className="tag">{product.category}</p>
-                  <h5 className="name">{product.name}</h5>
+                  <p className="tag" onClick={() => handleProductClick(product)}>{product.category}</p>
+                  <h5 className="name" onClick={() => handleProductClick(product)}>{product.name}</h5>
                   <p className="price">${product.price}</p>
                   <div className="towishlist">
                     <i className="fa-regular fa-heart"></i>
